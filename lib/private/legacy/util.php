4f396a049179604726310319f0f0b8be5aa73e6e
Vincent Petry <pvince81@owncloud.com>
2016-10-08 19:49:02 +0200

    Remove checks whether OC is running on Windows (#26304)
    There is a setup check that prevents installing on Windows.
    No need to keep the old unsupported Windows code paths.
    
    Also fix the posix test that was running the wrong verify method:

--- lib/private/legacy/util.php
+++ lib/private/legacy/util.php
@@ -663,6 +663,8 @@ class OC_Util {
 					'in <a href="%s">our documentation</a>.',
 					['https://owncloud.org/install/', 'owncloud.org/install/', 'https://owncloud.org/?p=8045'])
 			];
+			// don't bother with further checks
+			return $errors;
 		}
 
 		// Check if config folder is writable.
@@ -914,22 +916,18 @@ class OC_Util {
 	public static function checkDataDirectoryPermissions($dataDirectory) {
 		$l = \OC::$server->getL10N('lib');
 		$errors = [];
-		if (self::runningOnWindows()) {
-			//TODO: permissions checks for windows hosts
-		} else {
-			$permissionsModHint = $l->t('Please change the permissions to 0770 so that the directory'
-				. ' cannot be listed by other users.');
+		$permissionsModHint = $l->t('Please change the permissions to 0770 so that the directory'
+			. ' cannot be listed by other users.');
+		$perms = substr(decoct(@fileperms($dataDirectory)), -3);
+		if (substr($perms, -1) != '0') {
+			chmod($dataDirectory, 0770);
+			clearstatcache();
 			$perms = substr(decoct(@fileperms($dataDirectory)), -3);
-			if (substr($perms, -1) != '0') {
-				chmod($dataDirectory, 0770);
-				clearstatcache();
-				$perms = substr(decoct(@fileperms($dataDirectory)), -3);
-				if (substr($perms, 2, 1) != '0') {
-					$errors[] = [
-						'error' => $l->t('Data directory (%s) is readable by other users', [$dataDirectory]),
-						'hint' => $permissionsModHint
-					];
-				}
+			if (substr($perms, 2, 1) != '0') {
+				$errors[] = [
+					'error' => $l->t('Data directory (%s) is readable by other users', [$dataDirectory]),
+					'hint' => $permissionsModHint
+				];
 			}
 		}
 		return $errors;
@@ -945,7 +943,7 @@ class OC_Util {
 	public static function checkDataDirectoryValidity($dataDirectory) {
 		$l = \OC::$server->getL10N('lib');
 		$errors = [];
-		if (!self::runningOnWindows() && $dataDirectory[0] !== '/') {
+		if ($dataDirectory[0] !== '/') {
 			$errors[] = [
 				'error' => $l->t('Data directory (%s) must be an absolute path', [$dataDirectory]),
 				'hint' => $l->t('Check the value of "datadirectory" in your configuration')
@@ -1216,11 +1214,6 @@ class OC_Util {
 	 * @return bool
 	 */
 	public static function isSetLocaleWorking() {
-		// setlocale test is pointless on Windows
-		if (OC_Util::runningOnWindows()) {
-			return true;
-		}
-
 		\Patchwork\Utf8\Bootup::initLocale();
 		if ('' === basename('§')) {
 			return false;
